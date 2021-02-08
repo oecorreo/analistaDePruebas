fc690c31a661df8cf5131021f42815e0d745bf4e
oecorreo <73183821+oecorreo@users.noreply.github.com>
2021-02-08 16:48:37 -0500

    Add files via upload

---
+++ co.com.choucair.certification.proyectobase/src/main/java/co/com/choucair/certification/proyectobase/userinterface/ChoucairLoginPage.java
@@ -0,0 +1,18 @@
+package co.com.choucair.certification.proyectobase.userinterface;
+
+import net.serenitybdd.screenplay.targets.Target;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.By;
+
+public class ChoucairLoginPage extends PageObject {
+    public static final Target LOGIN_BUTTON = Target.the("button that shows us the form to login").located(By.xpath("//div[@class='d-none d-lg-block']//strong[contains(text(),'Ingresar')]"));
+
+    public static final Target INPUT_USER = Target.the("WHERE DO WE WRITE THE USER").located(By.id("username"));
+
+    public static final Target INPUT_PASSWORD = Target.the("where do we write the password").located(By.id("password"));
+
+    public static final Target ENTER_BUTTON = Target.the("button to confirm login").located(By.xpath("//button[contains(@class,'btn btn-primary')]"));
+
+
+
+}
---
+++ co.com.choucair.certification.proyectobase/src/test/resources/features/academyChoucair.feature
@@ -0,0 +1,22 @@
+# Autor: Brandon
+@stories
+
+  Feature: Academy Choucair
+    As a user, I want to learn how ro automate in screamplay at the Choucair Academy with the automation course
+
+    @scenario1
+    Scenario Outline: Search for a automation course
+      Given than brandon wants to learn automation at the academy Choucair
+        | strUser   |strPassword   |
+        | <strUser> |<strPassword> |
+
+      When he search for the course Recursos Automatizacion Bancolombia on the choucair academy platform
+        | strCourse   |
+        | <strCourse> |
+      Then he finds the course called resources Recursos Automatizacion Bancolombia
+        | strCourse   |
+        | <strCourse> |
+
+      Examples:
+        | srtUser     | strPassword   | strCourse |
+        | 1022427734  |  Choucair2021*| Recursos Automatizacion Bancolombia  |
